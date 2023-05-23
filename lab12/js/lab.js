3350793deb4b47c544d0ea6ae8895b8ec8faa39d
Kellen Zheng <kzheng8@ucsc.edu>
2023-05-23 00:37:24 -0700

    lab12x
    extra

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -7,25 +7,33 @@
 function sortingHat(userInput) {
   var len = userInput.length;
   var mod = len % 4;
-  if (mod == 0) {
-    return "Gryffindor";
-  }
-  else if (mod == 1) {
-    return "Hufflepuff";
-  }
-  else if (mod == 2) {
-    return "Ravenclaw";
-  }
-  else if (mod == 3) {
-    return "Slytherin";
-  }
-
+  //array that lists all the houses
+  var houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+  //return the associated house with the array element
+  return houses[mod];
 }
 
 function main() {
+  //adding event listener to the button
   $("#button").click(function(){
+    //variable that stores user input data
     var name = $("#userInput").val();
-    $("#output").html("The Sorting Hat has sorted you into " + sortingHat(name) + "!");
+    //prints the sortingHat output in the "output" <div>
+    $("#output").html("The Sorting Hat has sorted you into " + sortingHat(name) + "!" + "<br></br>");
+
+    if (sortingHat(name) == "Gryffindor") {
+      $("#output").append("The lion, a valiant symbol of courage and chivalry.");
+    }
+    else if (sortingHat(name) == "Hufflepuff") {
+      $("#output").append("The badger, a diligent mascot loyal to others.");
+    } 
+    else if (sortingHat(name) == "Ravenclaw") {
+      $("#output").append("The eagle, an intelligent being full of wit and knowledge.");
+    } 
+    else if (sortingHat(name) == "Slytherin") {
+      $("#output").append("The snake, a cunning character with an eye for ambition.");
+    }
+
   });
   
 }
