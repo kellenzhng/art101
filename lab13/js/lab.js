461abda58f1541fb33ce99dceee9b92bdffe0b92
Kellen Zheng <kzheng8@ucsc.edu>
2023-05-23 22:22:43 -0700

    lab13X
    X

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -3,21 +3,25 @@
 // Date: 5/23/23
 
 //function that iterates from 1-200
-//it outputs "Fizz", "Buzz", or "Boom" depending on the number factors
-function fizzBuzz() {
-  for (i=1; i<=200; i++) {
+//it outputs a custom phrase depending on the factor
+function fizzBuzz(loopCount, factorArray, phraseArray) {
+  for (i=1; i<=loopCount; i++) {
     var strOrder = " ";
     //if the number is a factor of 3, "Fizz"
-    if (i % 3 == 0) {
-      strOrder += "Fizz";
+    if (i % factorArray[0] == 0) {
+      strOrder += phraseArray[0];
     }
     //if the number is a factor of 5, "Buzz"
-    if (i % 5 == 0) {
-      strOrder += "Buzz";
+    if (i % factorArray[1] == 0) {
+      strOrder += phraseArray[1];
     }
     //if the number is a factor of 7, "Boom"
-    if (i % 7 == 0) {
-      strOrder += "Boom";
+    if (i % factorArray[2] == 0) {
+      strOrder += phraseArray[2];
+    }
+    //if the number is a factor of 9, "Bang"
+    if (i % factorArray[3] == 0) {
+      strOrder += phraseArray[3];
     }
     //if the number is not a factor, output the number
     if (strOrder == " ") {
@@ -29,7 +33,17 @@ function fizzBuzz() {
 }
 
 function main() {
-  fizzBuzz();
+  //event listener that starts the function based on input
+  $("#button").click(function(){
+    //variable holding user input of number of runs
+    var numLoops = $("#loopCount").val();
+    //array holding user input of factors
+    var factorArray = [$("#factorInput1").val(), $("#factorInput2").val(), $("#factorInput3").val(), $("#factorInput4").val()];
+    //array holding user input of phrases
+    var phraseArray = [$("#textInput1").val(), $("#textInput2").val(), $("#textInput3").val(), $("#textInput4").val()]
+    fizzBuzz(numLoops, factorArray, phraseArray);
+  })
+
 }
 
 
